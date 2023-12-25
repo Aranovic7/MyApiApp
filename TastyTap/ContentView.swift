12c5ee022dc48882a6b8954e8a8a07b68f165401
Aran Ali <Aliaran2717@gmail.com>
2023-12-25 12:22:32 +0100

    Initial Commit

---
+++ TastyTap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TastyTap
+//
+//  Created by Aran Ali on 2023-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TastyTap/TastyTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyTapApp.swift
+//  TastyTap
+//
+//  Created by Aran Ali on 2023-12-25.
+//
+
+import SwiftUI
+
+@main
+struct TastyTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
